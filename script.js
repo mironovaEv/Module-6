d4f62e304ff4d5ab5b96d89689acf0286339239d
mironovaEv <mironovaev26@gmail.com>
2022-03-20 23:13:00 +0700

    First alg

--- script.js
+++ script.js
@@ -5,37 +5,15 @@ window.onload = function() {
         document.getElementById('ez2').style.visibility = 'hidden';
         var n = document.getElementById('map_size').value;
         document.getElementById("container").innerHTML = "";
-        var QueryString = function() {
-            var query_string = {};
-            var query = window.location.search.substring(1);
-            var vars = query.split("&");
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split("=");
-                if (typeof query_string[pair[0]] === "undefined") {
-                    query_string[pair[0]] = pair[1];
-                } else if (typeof query_string[pair[0]] === "string") {
-                    var arr = [query_string[pair[0]], pair[1]];
-                    query_string[pair[0]] = arr;
-                } else {
-                    query_string[pair[0]].push(pair[1]);
-                }
-            }
-            return query_string;
-        }();
+
         if (n > 1 && n < 51) {
             var defaultNum = n;
         } else {
             var defaultNum = 0;
         }
-        var num = parseInt(QueryString.num) || defaultNum;
         var container = document.getElementById('container');
         var width = container.offsetWidth;
-        createGrid(num);
-
-        function changeColor(id) {
-            var sqr = getElementById(id);
-            sqr.style.backgroundColor = 'black';
-        }
+        createGrid(defaultNum);
 
         function createGrid(n) {
             if (isNaN(n) || n <= 0)
@@ -77,21 +55,15 @@ window.onload = function() {
         }
     }
     var count = 0;
-    var st_fn = [];
     document.querySelector("#start_finish").onclick = function() {
         count = 0;
         var n = document.getElementById('map_size').value;
         for (var i = 0; i < n * n; i++) {
             var sqr = document.getElementById(i);
-            if (sqr.style.background == "yellow") {
+            if (sqr.style.background == "yellow" || sqr.style.background == "red" || sqr.style.background == "green") {
                 sqr.style.background = "white";
             }
         }
-        for (let i = 0; i < st_fn.length; i++) {
-            let sqr = document.getElementById(st_fn[i]);
-            sqr.style.background = 'white';
-        }
-        st_fn = [];
         let elms = document.getElementsByClassName('gridCell'); {
 
             for (let i in elms) {
@@ -101,7 +73,6 @@ window.onload = function() {
                         if (count == 0) { this.style.background = 'red'; }
                         if (count == 1) { this.style.background = 'green'; }
                         count = count + 1;
-                        st_fn.push(i);
 
                     }
                 };
